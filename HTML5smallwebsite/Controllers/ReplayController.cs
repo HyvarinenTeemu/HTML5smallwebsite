b2d63c16470a4a5afe146f04b1681183f2175081
Teemu Hyvärinen <teemu.hyvarinen@stud.pointcollege.fi>
2018-05-14 15:09:47 +0300

    Added replay controller, modified first page. Removed unnecessary code

---
+++ HTML5smallwebsite/Controllers/ReplayController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTML5smallwebsite.Controllers
+{
+    public class ReplayController : Controller
+    {
+        public IActionResult Replays()
+        {
+            return View();
+        }
+    }
+}
